3cb2b6203bf3627426add1387c8a56173470a60e
fluffycookiedough <fluffy_cookie_dough@naver.com>
2025-06-19 15:08:59 +1000

    ValidateStep function uses switch now

--- front-end/src/pages/ProfileSetup.jsx
+++ front-end/src/pages/ProfileSetup.jsx
@@ -69,31 +69,30 @@ const ProfileSetup = () => {
 
   // Validate form data for each step
   const validateStep = (step, profileData) => {
-    if (step === 1) {
-      if (!profileData.fullName) return "Full name is required";
-      if (!profileData.address) return "Address is required";
-      if (!profileData.email || !profileData.email.includes("@"))
-        return "A valid email is required";
-      if (!profileData.phoneNumber) return "Phone number is required";
-    }
+    switch(step) {
+      case 1:
+        if (!profileData.fullName) return "Full name is required";
+        if (!profileData.address) return "Address is required";
+        if (!profileData.email || !profileData.email.includes("@"))
+          return "A valid email is required";
+        if (!profileData.phoneNumber) return "Phone number is required";
+        break;
+      case 2:
+        if (profileData.company === "Yes") {
+          if (profileData.companyDetails.acn.toString().length !== 9)
+            return "ACN must be 9 digits";
 
-    if (step === 2) {
-   
-      if (profileData.company === "Yes") {
-        if (profileData.companyDetails.acn.toString().length !== 9)
-          return "ACN must be 9 digits";
+          if (!profileData.companyDetails.companyName) 
+            return "Company name is required";
+        }
+        break;
+      case 3:
+        if (!profileData.abn) return "ABN is required";
+        if (profileData.abn.toString().length !== 11)
+          return "ABN must be 11 digits";
 
-        if (!profileData.companyDetails.companyName) 
-          return "Company name is required";
-      }
-    }
-
-    if (step === 3) {
-      if (!profileData.abn) return "ABN is required";
-      if (profileData.abn.toString().length !== 11)
-        return "ABN must be 11 digits";
-
-      if (!profileData.brn) return "Builder Registration  is required";
+        if (!profileData.brn) return "Builder Registration  is required";
+        break;
     }
 
     return null;
