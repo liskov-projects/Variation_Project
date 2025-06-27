2a5e7666e57929f0b6153cc6ac1502fd731dcf2a
Poorvith <poorvithgowda10@gmail.com>
2025-06-27 14:23:18 +1000

    wip

--- back-end/index.js
+++ back-end/index.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
