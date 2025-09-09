4bb0130b06a818b7c44059bbb8ebe6248c488c56
verrakav <verrafalenko@gmail.com>
2025-09-09 12:47:02 +0930

    added: a very basic test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,6 +1,15 @@
 describe("Basic test", () => {
     it("loads the home page", () => {
-        cy.visit("/welcome");
-        cy.contains("Welcome").should("exist")
-    })
+        // Visit the public home page and check for actual content
+        cy.visit("/");
+        cy.contains("Innovate.").should("exist");
+        cy.contains("Create").should("exist");
+        cy.contains("Transform").should("exist");
+    });
+    
+    it("loads the sign-in page", () => {
+        // Test the sign-in page which should be accessible
+        cy.visit("/sign-in");
+        cy.url().should("include", "/sign-in");
+    });
 });
